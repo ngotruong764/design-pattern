a29369f5c497adba8cd60eadf6021efde93f275d
truongnq1.ho <truongnq1.ho@mcredit.com.vn>
2026-08-09 08:59:34 +0700

    Add open-close principle

---
+++ src/principle/openclose/PhoneSubscriber.java
@@ -0,0 +1,15 @@
+package principle.openclose;
+
+import java.util.List;
+
+public class PhoneSubscriber extends Subscriber {
+
+    // open for modification
+    @Override
+    public double calculateBill() {
+        List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(subscriberId);
+        long totalDuration = sessions.stream().mapToLong(CallHistory.Call::getDuration).sum();
+        return totalDuration*baseRate/100;
+    }
+
+}
